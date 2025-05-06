b47bab77ebf1107b518bf795eb9407ff74dbaf17
NaregM <nareg.mirzatuny@gmail.com>
2025-05-06 01:29:55 -0700

    some results

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # nominative_determinism_llm_agent
 Using large language models to study nominative determinism.
+
+{'p_obs': 0.006,
+ 'p_null_median': 0.01,
+ 'p_value': 0.9,
+ 'delta': -0.004,
+ 'risk_ratio': 0.6,
+ 'null_distribution': array([0.01 , 0.008, 0.009, 0.013, 0.011, 0.009, 0.007, 0.006, 0.011,
+        0.005, 0.011, 0.012, 0.007, 0.011, 0.011, 0.005, 0.01 , 0.009,
+        0.015, 0.012, 0.007, 0.005, 0.01 , 0.01 , 0.008, 0.012, 0.008,
+        0.01 , 0.01 , 0.009])}
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from helpers import run_nd_analysis
 from permutation import permutation_test, binary_score
 
 from pprint import pprint
+
 # ------------------------------------------------------------
 # ------------------------------------------------------------
 # ------------------------------------------------------------
@@ -14,8 +15,10 @@ if __name__ == "__main__":
     
     p = argparse.ArgumentParser()
     p.add_argument("--input_csv", required=True, help="path to input CSV")
-    #p.add_argument("--output_csv", required=True, help="path to write results CSV")
+    p.add_argument("--output_csv", required=False, help="path to write results CSV")
+    p.add_argument("--N", required=False, help="Sample size", type=int)
+    p.add_argument("--M", required=False, help="Permutation Size", type=int)
 
     args = p.parse_args()
-    pprint(permutation_test(args.input_csv, binary_score))
+    pprint(permutation_test(args.input_csv, binary_score, args.N, args.M))
     #run_nd_analysis(args.input_csv, args.output_csv)
